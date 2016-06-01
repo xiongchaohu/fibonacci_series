8094e92cc773cc0a908d7b6b46139e1adb1c6605
xiongchaohu <1732139715@qq.com>
2016-06-01 16:49:18 +0800

    'commit'

--- src/fibonacci_series.js
+++ src/fibonacci_series.js
@@ -2,6 +2,21 @@
 
 function fibonacci_series(n) {
 
+	var temp=[0,1]; 
+  
+    if(n==0)
+    	temp.pop();
+    else  
+    {  
+       	for (var i = 2; i < n+1; i ++)  
+       	{  
+           	tmp = temp[i-2] + temp[i-1];  
+           	temp.push(tmp); 
+        }  
+        
+      
+	}
+ 	return temp; 
 }
 
 module.exports = fibonacci_series;
